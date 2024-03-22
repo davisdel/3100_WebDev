d7f019fc58524d688fdddac19ee13ce66735181e
davis <davisdeloach@gmail.com>
2024-03-22 08:48:52 -0500

    3/22/24

--- nodeExample/index.js
+++ nodeExample/index.js
@@ -6,26 +6,27 @@ console.log("Listening on port " + HTTP_PORT);
 var app = express();
 app.use(cors());
 
+class Fruit {
+
+    constructor(strName, strColor){
+        this.name = strName;
+        this.color = strColor;
+    }
+
+}
+
 var arrFruit = [];
-let objBanana = {name:'banana', color:'yellow'};
-let objGrape = {name:'grape', color:'purple'};
-let objApple = {name:'apple', color:'red'};
+let objBanana = new Fruit('banana', 'yellow');
+let objGrape = new Fruit('apple','red');
+let objApple = new Fruit('grape', 'purple');
 arrFruit.push(objBanana);
 arrFruit.push(objApple);
 arrFruit.push(objGrape);
+arrFruit.push(new Fruit('kiwi', 'brown'));
 
 app.get("/", (req,res,next) => {
 
-    let strFruit = req.query.fruit;
-    console.log("Routed to base route");
-    console.log(strFruit);
-    arrFruit.forEach(function(thisfruit){
-        if(thisfruit.name == strFruit){
-            res.status(200).send(thisfruit);
-        }
-    })
-
-    res.status(200).send('{"message":"Fruit not found"}');
+    res.status(200).send(arrFruit);
 
 });
 
@@ -36,6 +37,51 @@ app.get("/hello", (req,res,next) => {
     console.log(strFruit);
 
     res.status(200).send("Hello " + strFruit);
+
+});
+
+app.post("/fruit", (req,res,next) => {
+
+    let strName = req.query.name;
+    let strColor = req.query.color;
+
+    arrFruit.push(new Fruit(strName, strColor));
+
+    res.json({'status':200,"message":arrFruit})
+
+});
+
+app.get("/fruit", (req,res,next) => {
+    
+    let strName = req.query.name;
+    if(strName){
+        arrFruit.forEach(function(fruit){
+            if(fruit.name == strName){
+                res.json({"status":200,"message":fruit});
+            }
+        })
+        res.json({"status":200,"message":"Fruit not found"});
+    } else {
+        res.json({"status":200,"message":arrFruit});
+    }
+
+});
+
+app.delete("/fruit", (req,res,next) => {
+    
+    let strName = req.query.name;
+    if(strName){
+        arrFruit.forEach(function(fruit,index){
+            if(fruit.name == strName){
+                arrFruit.splice(index,1);
+                res.json({"status":200,"message":fruit});
+            }
+        })
+        res.json({"status":200,"message":"Fruit not found"});
+    } else {
+        arrFruit = [];
+        res.json({"status":200,"message":arrFruit});
+    }
 
 });
 
